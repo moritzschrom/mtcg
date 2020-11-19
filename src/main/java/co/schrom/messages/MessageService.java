96bec514ea8789f2f4d4e7735ff1dc4cc1224f18
Moritz Schrom <moritz.schrom@schrom.co>
2020-11-19 06:48:18 +0100

    feat: Moved from runtime storage to database for persisting data
    Added the MySQL connector in Maven's pom.xml.
    Re-coded the MessageService to deal with database queries instead of ArrayLists.
    Fixed some bad HTTP responses (wrong Content-Type caused formatting issues in libraries such as Postman).

--- src/main/java/co/schrom/messages/MessageService.java
+++ src/main/java/co/schrom/messages/MessageService.java
@@ -1,54 +1,15 @@
 package co.schrom.messages;
 
-import lombok.Getter;
+import co.schrom.database.DatabaseService;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MessageService implements MessageServiceInterface {
-    @Getter
-    List<MessageInterface> messages;
-
-    private int id;
-
     private static MessageService instance;
 
     private MessageService() {
-        this.id = 0;
-        this.messages = new ArrayList<>();
-    }
-
-    public int nextId() {
-        id++;
-        return id;
-    }
-
-    @Override
-    public MessageInterface getMessage(int id) {
-        for (MessageInterface message : messages) {
-            if (message.getId() == id) {
-                return message;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public boolean addMessage(MessageInterface message) {
-        return messages.add(message);
-    }
-
-    @Override
-    public boolean replaceMessage(int id, MessageInterface message) {
-        MessageInterface old = this.getMessage(id);
-        if (old == null) return false;
-        messages.set(messages.indexOf(old), message);
-        return true;
-    }
-
-    @Override
-    public boolean removeMessage(int id) {
-        return messages.removeIf(message -> message.getId() == id);
     }
 
     public static MessageService getInstance() {
@@ -56,5 +17,134 @@ public class MessageService implements MessageServiceInterface {
             MessageService.instance = new MessageService();
         }
         return MessageService.instance;
+    }
+
+    @Override
+    public MessageInterface getMessage(int id) {
+        try {
+            Connection conn = DatabaseService.getInstance().getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT id, message FROM messages WHERE id=?;");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+
+            if (rs.next()) {
+                MessageInterface message = Message.builder()
+                        .id(rs.getInt(1))
+                        .message(rs.getString(2))
+                        .build();
+
+                rs.close();
+                ps.close();
+                conn.close();
+
+                return message;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<MessageInterface> getMessages() {
+        try {
+            Connection conn = DatabaseService.getInstance().getConnection();
+            Statement sm = conn.createStatement();
+            ResultSet rs = sm.executeQuery("SELECT id, message FROM messages;");
+
+            List<MessageInterface> messages = new ArrayList<>();
+            while (rs.next()) {
+                messages.add(Message.builder()
+                        .id(rs.getInt(1))
+                        .message(rs.getString(2))
+                        .build());
+            }
+
+            rs.close();
+            sm.close();
+            conn.close();
+
+            return messages;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public MessageInterface addMessage(MessageInterface message) {
+        try {
+            Connection conn = DatabaseService.getInstance().getConnection();
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO messages(message) VALUES(?);", Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, message.getMessage());
+
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows == 0) {
+                return null;
+            }
+
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return this.getMessage(generatedKeys.getInt(1));
+                }
+            }
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public MessageInterface updateMessage(int id, MessageInterface message) {
+        try {
+            Connection conn = DatabaseService.getInstance().getConnection();
+            PreparedStatement ps = conn.prepareStatement("UPDATE messages SET message = ? WHERE id = ?;");
+
+            ps.setString(1, message.getMessage());
+            ps.setInt(2, id);
+
+            int affectedRows = ps.executeUpdate();
+
+            ps.close();
+            conn.close();
+
+            if (affectedRows == 0) {
+                return null;
+            }
+
+            return this.getMessage(message.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteMessage(int id) {
+        try {
+            Connection conn = DatabaseService.getInstance().getConnection();
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM messages WHERE id = ?;");
+            ps.setInt(1, id);
+
+            int affectedRows = ps.executeUpdate();
+
+            ps.close();
+            conn.close();
+
+            if (affectedRows == 0) {
+                return false;
+            }
+
+            ps.close();
+            conn.close();
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
