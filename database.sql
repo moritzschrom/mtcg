6782f39c58064279881f2435950d681978b9c546
Moritz Schrom <moritz.schrom@schrom.co>
2021-01-09 22:55:15 +0100

    database: Added tables for the battle
    This includes the battle table, and the battle_rounds table for managing battles and storing the results.

--- database.sql
+++ database.sql
@@ -46,3 +46,29 @@ CREATE TABLE cards
     CONSTRAINT fk_package FOREIGN KEY (package_id) REFERENCES packages (id),
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+/* Battle */
+
+DROP TABLE IF EXISTS battle CASCADE;
+CREATE TABLE battle
+(
+    id       SERIAL PRIMARY KEY,
+    player_1 INT,
+    player_2 INT,
+    winner   INT,
+    CONSTRAINT fk_player_1 FOREIGN KEY (player_1) REFERENCES users (id),
+    CONSTRAINT fk_player_2 FOREIGN KEY (player_2) REFERENCES users (id),
+    CONSTRAINT winner FOREIGN KEY (winner) REFERENCES users (id)
+);
+
+DROP TABLE IF EXISTS battle_rounds CASCADE;
+CREATE TABLE battle_rounds
+(
+    id          SERIAL PRIMARY KEY,
+    card_1      INT NOT NULL,
+    card_2      INT NOT NULL,
+    winner_card INT NOT NULL,
+    CONSTRAINT fk_card_1 FOREIGN KEY (card_1) REFERENCES cards (id),
+    CONSTRAINT fk_card_2 FOREIGN KEY (card_2) REFERENCES cards (id),
+    CONSTRAINT fk_winner_card FOREIGN KEY (winner_card) REFERENCES cards (id)
+);
